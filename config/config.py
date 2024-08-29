37c9f653dc862175ea37d4604a5f086d9cd1355a
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-29 15:43:12 +0200

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,7 @@
+import os
+
+DATA_PATH = 'data/'
+RAW_DATA_PATH = os.path.join(DATA_PATH, 'raw/')
+PROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'processed/')
+LOGS_PATH = 'logs/'
+RESULTS_PATH = 'results/'
